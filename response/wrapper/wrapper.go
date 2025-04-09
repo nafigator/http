35482981bf5565214b5e61e1a0d14d6a3ba5139f
Yancharuk Alexander <alex@itvault.info>
2025-04-09 14:22:38 +0300

    chore: Response wrapper cleanup

--- response/wrapper/wrapper.go
+++ response/wrapper/wrapper.go
@@ -26,25 +26,25 @@ func New(w http.ResponseWriter, r *http.Request) Wrapper {
 }
 
 // Write function overwrites the http.ResponseWriter Write() function.
-func (rww *Wrapper) Write(buf []byte) (int, error) {
-	rww.r.Body = io.NopCloser(bytes.NewBuffer(buf))
+func (w *Wrapper) Write(buf []byte) (int, error) {
+	w.r.Body = io.NopCloser(bytes.NewBuffer(buf))
 
-	return rww.w.Write(buf)
+	return w.w.Write(buf)
 }
 
 // Header function overwrites the http.ResponseWriter Header() function.
-func (rww *Wrapper) Header() http.Header {
-	return rww.w.Header()
+func (w *Wrapper) Header() http.Header {
+	return w.w.Header()
 }
 
 // WriteHeader function overwrites the http.ResponseWriter WriteHeader() function.
-func (rww *Wrapper) WriteHeader(statusCode int) {
-	rww.r.StatusCode = statusCode
+func (w *Wrapper) WriteHeader(statusCode int) {
+	w.r.StatusCode = statusCode
 
-	rww.w.WriteHeader(statusCode)
+	w.w.WriteHeader(statusCode)
 }
 
 // Result returns response.
-func (rww *Wrapper) Result() *http.Response {
-	return &rww.r
+func (w *Wrapper) Result() *http.Response {
+	return &w.r
 }
